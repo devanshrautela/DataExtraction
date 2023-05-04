98537f360c864df5a912391ac782b1a32162e9bb
deepanshu <90817042+Deepanshupanwar@users.noreply.github.com>
2023-05-04 12:07:02 +0530

    Update RuleBased.py

--- RuleBased.py
+++ RuleBased.py
@@ -1,34 +1,10 @@
-
-import re
-
-# define the regular expressions for identifying cancer-related terms
-cancer_regex = re.compile(r"(cancer|tumor|carcinoma|neoplasm|metastasis)")
-breast_cancer_regex = re.compile(r"(breast cancer|breast carcinoma)")
-lung_cancer_regex = re.compile(r"(lung cancer|lung carcinoma)")
-
-# sample medical text
-with open('cancer.txt','r') as f:
-    text = "The patient was diagnosed with lung cancer, stage 3. Treatment included chemotherapy and radiation therapy."
-
-# create a dictionary to store the extracted data
-data = {}
-
-# check for lung cancer
-if lung_cancer_regex.search(text):
-    data["cancer_type"] = "lung cancer"
-    data["stage"] = 3
-    data["treatments"] = ["chemotherapy", "radiation therapy"]
-    
-print(data)
-
-#deepanshu's code
 import re
 
 # Define regular expression to match cancer-related terms
 cancer_regex = re.compile(r'cancer|malignancy', re.IGNORECASE)
 
 # Read in unstructured text data from file
-with open('impure_test.txt', 'r',encoding='utf-8') as f:
+with open('impure_test_unstructured2.txt', 'r',encoding='utf-8') as f:
     text = f.read()
 
 # Apply rules to extract paragraphs containing cancer-related terms
